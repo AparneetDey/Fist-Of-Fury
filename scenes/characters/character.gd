9fa165394c57713e72936e3c8dee2fc6040a53d2
Aparneet Dey <aparnitd@gmail.com>
2025-12-13 16:10:32 +0530

    Add animation and movement logic to character
    Introduced state-based movement and animation handling in character.gd, including input processing, animation switching, and sprite flipping. Updated player.tscn to include AnimationPlayer with idle and walk animations for the character sprite.

--- scenes/characters/character.gd
+++ scenes/characters/character.gd
@@ -4,6 +4,37 @@ extends CharacterBody2D
 @export var damage : int
 @export var speed : float
 
+@onready var animatedSprite := $AnimationPlayer
+@onready var characterSprite := $CharacterSprite
+
+enum State { IDLE, WALK }
+var state := State.IDLE
+
 func _process(delta: float) -> void:
+	handleMovement()
+	handleInput()
+	handleAnimation()
+	flipCharacter()
+	move_and_slide()
+
+func handleMovement() -> void:
+	if velocity.length() == 0:
+		state = State.IDLE
+	else:
+		state = State.WALK
+
+func handleInput() -> void:
 	var direction := Input.get_vector("left", "right", "up", "down")
-	position += delta*direction*speed
+	velocity = direction*speed
+	
+func handleAnimation() -> void:
+	if state == State.IDLE:
+		animatedSprite.play("idle")
+	elif  state == State.WALK:
+		animatedSprite.play("walk")
+
+func flipCharacter() -> void:
+	if velocity.x > 0:
+		characterSprite.flip_h = false
+	elif velocity.x < 0:
+		characterSprite.flip_h = true
