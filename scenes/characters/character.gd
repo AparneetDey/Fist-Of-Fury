e669d5bff0c0899819b917c33787ce62db5a97c1
Aparneet Dey <aparnitd@gmail.com>
2025-12-14 13:13:35 +0530

    Add jump and air state logic to character controller
    Introduces jump, takeoff, and land states to the character controller, including new animations and height-based movement. Updates player scene with corresponding animations and parameters, and adjusts world scene to set player damage and health to zero.

--- scenes/characters/character.gd
+++ scenes/characters/character.gd
@@ -1,23 +1,39 @@
 extends CharacterBody2D
 
+const GRAVITY := 600.0
+
 @export var damage : int
 @export var health : int
+@export var heightIntensity : float
 @export var speed : float
 
 @onready var animatedSprite := $AnimationPlayer
 @onready var characterSprite := $CharacterSprite
 @onready var damageEmitter := $DamageEmitter
 
-enum State { IDLE, WALK, ATTACK }
+enum State { IDLE, WALK, ATTACK, TAKEOFF, JUMP, LAND }
+
+var animMap = {
+	State.IDLE: "idle",
+	State.WALK: "walk",
+	State.ATTACK: "punch",
+	State.TAKEOFF: "takeoff",
+	State.JUMP: "jump",
+	State.LAND: "land",
+}
 var state := State.IDLE
+var height := 0.0
+var heightSpeed := 0.0
 
 func _ready() -> void:
 	damageEmitter.area_entered.connect(onEmitDamage.bind())
 
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	handleMovement()
 	handleInput()
 	handleAnimation()
+	handleAirTime(delta)
+	characterSprite.position = Vector2.UP * height
 	flipCharacter()
 	move_and_slide()
 
@@ -33,14 +49,21 @@ func handleInput() -> void:
 	velocity = direction*speed
 	if canAttack() && Input.is_action_just_pressed("attack"):
 		state = State.ATTACK
+	if canJump() && Input.is_action_just_pressed("jump"):
+		state = State.TAKEOFF
 	
 func handleAnimation() -> void:
-	if state == State.IDLE:
-		animatedSprite.play("idle")
-	elif  state == State.WALK:
-		animatedSprite.play("walk")
-	elif state == State.ATTACK:
-		animatedSprite.play("punch")
+	if animatedSprite.has_animation(animMap[state]):
+		animatedSprite.play(animMap[state])
+		
+func handleAirTime(delta : float) -> void:
+	if state == State.JUMP:
+		height += heightSpeed * delta
+		if height < 0:
+			height = 0
+			state = State.LAND
+		else:
+			heightSpeed -= GRAVITY * delta
 
 func flipCharacter() -> void:
 	if velocity.x > 0:
@@ -56,7 +79,17 @@ func canMove() -> bool:
 func canAttack() -> bool:
 	return state == State.IDLE or state == State.WALK
 	
+func canJump() -> bool:
+	return state == State.IDLE or state == State.WALK
+	
 func onActionComplete() -> void:
+	state = State.IDLE
+	
+func onTakeOffComplete() -> void:
+	state = State.JUMP
+	heightSpeed = heightIntensity
+	
+func onLandComplete() -> void:
 	state = State.IDLE
 
 func onEmitDamage(damageReceiver : DamageReceiver) -> void:
