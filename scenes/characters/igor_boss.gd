3bcc52ae887144562286e6fff5d28071df049ffc
Aparneet Dey <aparnitd@gmail.com>
2025-12-31 21:46:14 +0530

    Enhance IgorBoss AI and balance boss parameters
    Added attack cooldown and vulnerability durations, improved knockback and state handling, and refined AI logic for IgorBoss. Updated scene parameters to increase boss health, damage, and speed, and added a ProjectileAim node for attack checks.

--- scenes/characters/igor_boss.gd
+++ scenes/characters/igor_boss.gd
@@ -1,8 +1,20 @@
 class_name IgorBoss
 extends Character
 
+const GROUND_FRICTION := 50
+
 @export var DistanceFromPlayer : int
+@export var DurationBetweenAttacks : int
+@export var DurationVulnerable : int
 @export var player : Player
+
+var knockbackForce := Vector2.ZERO
+var timeSinceLastAttacked := Time.get_ticks_msec()
+var timeSinceVulnerable := Time.get_ticks_msec()
+
+func _process(delta: float) -> void:
+	super._process(delta)
+	knockbackForce = knockbackForce.move_toward(Vector2.ZERO, delta * GROUND_FRICTION)
 
 func getTargetPosition() -> Vector2:
 	var target := Vector2.ZERO
@@ -18,16 +30,65 @@ func isPlayerWithInRange() -> bool:
 
 func handleInput() -> void:
 	if player != null and canMove():
-		if isPlayerWithInRange():
-			velocity = Vector2.ZERO
-			state = State.IDLE
+		if canAttack() and projectileAim.is_colliding():
+			state = State.FLY
+			velocity = heading * FlightSpeed
 		else:
-			var targetPosition := getTargetPosition()
-			var direction = (targetPosition - position).normalized()
-			velocity = direction * Speed
-			state = State.WALK
+			if isPlayerWithInRange():
+				velocity = Vector2.ZERO
+				state = State.IDLE
+			else:
+				var targetPosition := getTargetPosition()
+				var direction = (targetPosition - position).normalized()
+				velocity = (direction + knockbackForce) * Speed
+				state = State.WALK
+
+func handleGroundedTime() -> void:
+	if state == State.GROUNDED and currentHealth > 0:
+		state = State.RECOVER
+		timeSinceVulnerable = Time.get_ticks_msec()
+	elif state == State.RECOVER and (Time.get_ticks_msec() - timeSinceVulnerable) > DurationVulnerable:
+		state = State.IDLE
+		timeSinceLastAttacked = Time.get_ticks_msec()
 
 func setHeading() -> void:
-	if player == null and not canMove():
+	if player != null and canMove():
+		heading = Vector2.LEFT if position.x > player.position.x else Vector2.RIGHT
+
+func canGetHurt() -> bool:
+	return true
+
+func canAttack() -> bool:
+	if (Time.get_ticks_msec() - timeSinceLastAttacked) < DurationBetweenAttacks:
+		return false
+	return super.canAttack()
+
+func isVulnerable() -> bool:
+	return state == State.RECOVER
+
+func onActionComplete() -> void:
+	if state == State.HURT:
+		state = State.RECOVER
 		return
-	heading = Vector2.LEFT if position.x > player.position.x else Vector2.RIGHT
+	super.onActionComplete()
+
+func onReceiveDamage(damage : int, direction : Vector2, _hitType: DamageReceiver.HitType) -> void:
+	if not isVulnerable():
+		knockbackForce = direction * KnockbackIntensity
+		return
+	currentHealth = clamp(currentHealth - damage, 0, MaxHealth)
+	if currentHealth <= 0:
+		state = State.FALL
+		heightSpeed = KnockdownIntensity
+		velocity = direction * KnockbackIntensity
+	else:
+		velocity = Vector2.ZERO
+		state = State.HURT
+
+func onEmitDamage(receiver : DamageReceiver) -> void:
+	receiver.damageReceived.emit(Damage, heading, DamageReceiver.HitType.KNOCKDOWN)
+	state = State.IDLE
+	timeSinceLastAttacked = Time.get_ticks_msec()
+
+func isAttacking() -> bool:
+	return state == State.FLY
